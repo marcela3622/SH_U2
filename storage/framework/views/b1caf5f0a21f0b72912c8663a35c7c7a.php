66e5fddb46eff949f2fcd32846a6575c105206b3
GaelQuintero <tober223@outlook.com>
2024-10-16 19:51:56 -0700

    Carga de archivos a la rama develop

---
+++ storage/framework/views/b1caf5f0a21f0b72912c8663a35c7c7a.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Inicio de sesión</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+<?php echo $__env->yieldContent('contenido'); ?>
+<div class="container-sm">
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <div class="card-header text-center p-3 mb-2 bg-primary text-white">
+           Iniciar sesión &nbsp; <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-video" viewBox="0 0 16 16">
+            <path d="M8 9.05a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+            <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zm10.798 11c-.453-1.27-1.76-3-4.798-3-3.037 0-4.345 1.73-4.798 3H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1z"/>
+          </svg>
+          </div>
+        <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Correo electronico</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <div id="emailHelp" class="form-text">Nunca compartas tus datos personales con nadie.</div>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+              <input type="password" class="form-control" id="inputPassword" >
+              <br>
+              <!-- An element to toggle between password visibility -->
+              <div class="form-check" id="showCheck" hidden>
+                <input class="form-check-input" type="checkbox"  onclick="viewPassword()">
+                <label class="form-check-label" for="flexCheckDefault">
+                  Mostrar contraseña  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
+  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
+</svg>
+                </label>
+              </div>
+              <script>
+function viewPassword() {
+  var x = document.getElementById("inputPassword");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+document.getElementById("inputPassword").addEventListener("input", function() {
+  var inputValue = this.value;
+  var showCheck = document.getElementById("showCheck");
+  
+  if (inputValue === "") {
+    showCheck.hidden = true;  // Ocultar elemento
+  } else {
+    showCheck.hidden = false; // Mostrar elemento
+  }
+});
+
+              </script>
+              
+            </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary ">Acceder <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
+                    <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+                  </svg></button>
+            </div>
+           
+          </form>
+          <br>
+          <div class="card-footer text-body-secondary text-center">
+            PCP Software Industries ©
+          </div>
+
+    </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/gestor_empleados/resources/views/welcome.blade.php ENDPATH**/ ?>
